982df0f7e6c0872d970cb7973e2d4f77a462cf76
otmane <maacha98otmane@gmail.com>
2022-03-25 11:56:03 +0100

    Add Relation to Model (Product & Category)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function sub_categories()
+    {
+        return $this->hasMany(Category::class, 'parent_category');
+    }
 }
